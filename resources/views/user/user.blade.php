649d2c03551ac5822c12a376ef2951dbc49dc21e
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2025-02-05 12:23:11 +0700

    Update user.blade.php

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section( 'title', 'User')
+
 @section('content')
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
@@ -12,7 +13,7 @@
                 </div>
             @endif
             @if($errors->any())
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
@@ -24,27 +25,26 @@
             <div class="d-flex justify-content-end mb-3">
                 <a href="/tambah-user" class="btn btn-primary"><i class="fa fa-plus"></i> Kasir Baru</a>
             </div>
-            <table class="table">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Password</th>
-                        <th scope="col">Role</th>
-                        <th scope="col">*Set</th>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Option</th>
                     </tr>
                 </thead>
+                
                 <tbody>
-                    @foreach ($user as $item)
+                    @foreach ($user as $index => $item)
                         <tr>
-                            <td >{{ $loop->iteration }}</td>
+                            <td>{{ $index + 1 }}</td>
                             <td>{{ $item->name }}</td>
                             <td>{{ $item->email }}</td>
-                            <td>{{ $item->password }}</td>
                             <td>{{ $item->role }}</td>
                             <td>
-                                <a class="btn btn-success btn-sm" href="/edit-user={{ $item->id }}"><i class="fa fa-edit"></i> Edit</a>
+                                <a class="btn btn-success btn-sm" href="/edit-user/{{ $item->id }}"><i class="fa fa-edit"></i> Edit</a>
                                 <a class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin menghapus item ini?')" href="/hapus-user/{{ $item->id }}"><i class="fa fa-trash"></i> Hapus</a>
                             </td>
                         </tr>
@@ -55,3 +55,18 @@
     </div>
 </div>
 @endsection
+
+@section('scripts')
+<!-- DataTables -->
+<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable();
+    });
+</script>
+@endsection
+
